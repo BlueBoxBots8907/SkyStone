c39097489d8664b1d70b359363f6e19b52a0f15b
Nathan7722 <60716270+Nathan7722@users.noreply.github.com>
2020-02-15 10:16:58 -0600

    Update FieldUtil.kt
    attempt to create angled skystone collection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/kotlinauto/FieldUtil.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/kotlinauto/FieldUtil.kt
@@ -1,11 +1,11 @@
-package org.firstinspires.ftc.teamcode.autos.kotlinauto
+package auto
 
 import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.geometry.Vector2d
 import com.acmerobotics.roadrunner.util.Angle
+import com.sun.org.apache.xpath.internal.operations.*
 
-fun Double.toRadians(): Double = Math.toRadians(this)
-
-public object FieldUtil {
+object FieldUtil {
     enum class Color {
         RED {
             override operator fun not() = BLUE
@@ -25,63 +25,45 @@ public object FieldUtil {
         abstract operator fun not(): Side
     }
 
-    @JvmStatic
     fun flip(pose: Pose2d) =
-            Pose2d(
-                    pose.x, -pose.y, Angle.norm(-pose.heading)
-            )
+        Pose2d(
+            pose.x, -pose.y, Angle.norm(-pose.heading)
+        )
 
-    @JvmStatic
     fun getStartingPose(color: Color, side: Side, doesPull: Boolean): Pose2d {
         val pose = Pose2d(
-                if (side == FieldUtil.Side.STONE) -33.0 else 33.0, 63.0, if (side == FieldUtil.Side.FOUNDATION && doesPull) Math.toRadians(90.0) else Math.toRadians(270.0)
+            if (side == FieldUtil.Side.STONE) -33.0 else 33.0, 63.0, if (side == FieldUtil.Side.FOUNDATION && doesPull) Math.toRadians(90.0) else Math.toRadians(270.0)
         )
         return if (color == FieldUtil.Color.RED) flip(pose) else pose
     }
-
-    @JvmStatic
     fun getStonePose(color: Color, index: Int): Pose2d {
         val pose = Pose2d(
-                -26.0 - index * 8.0 - 1.0,
-                36.0,
-                Math.toRadians(270.0)
+            -24.0 - index * 8.0 + (if (index == 5) 8.0 else 0.0),
+            26.5 + (if (index == 5) 1.0 else 0.0),
+            if (index == 5) Math.toRadians(195.0) else Math.toRadians(230.0)
         )
         return if (color == FieldUtil.Color.RED) flip(pose) else pose
     }
-
-    @JvmStatic
-    fun getStonePoseFive(color: Color): Pose2d {
-        val pose = Pose2d(-60.0, 27.0, Math.toRadians(225.0))
-        return if (color == FieldUtil.Color.RED) flip(pose) else pose
-    }
-
-    @JvmStatic @JvmOverloads
-    fun getSkybridgePose(color: Color, side: Side, avoidBridge: Boolean = false): Pose2d {
+    fun getSkybridgePose(color: Color, side: Side): Pose2d {
         val pose = Pose2d(
-                0.0, if (side == FieldUtil.Side.STONE) 40.0 + (if (avoidBridge) 5.0 else 0.0) else 60.0, Math.toRadians(180.0)
+            0.0, if (side == FieldUtil.Side.STONE) 37.0 else 60.0, Math.toRadians(180.0)
         )
         return if (color == FieldUtil.Color.RED) flip(pose) else pose
     }
-
-    @JvmStatic
     fun getFoundationPose(color: Color): Pose2d {
         val pose = Pose2d(
-                45.0, 30.0, Math.toRadians(97.0)
+            45.0, 29.0, Math.toRadians(97.0)
         )
         return if (color == FieldUtil.Color.RED) flip(pose) else pose
     }
-
-    @JvmStatic
     fun getDroppingPose(color: Color): Pose2d {
         val pose = Pose2d(
-                45.0, 42.0, Math.toRadians(180.0)
+            45.0, 40.0, Math.toRadians(180.0)
         )
         return if (color == FieldUtil.Color.RED) flip(pose) else pose
     }
-
-    @JvmStatic
     fun getFoundationReleasePose(color: Color): Pose2d {
-        val pose = Pose2d(33.0, 46.0, Math.toRadians(180.0))
+        val pose = Pose2d(35.0, 43.0, Math.toRadians(180.0))
         return if (color == FieldUtil.Color.RED) flip(pose) else pose
     }
 }
